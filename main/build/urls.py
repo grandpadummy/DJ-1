99e9a0b7e09d5663a93fce34de541e7e3fe9eb31
clear <grandpadummy911@gmail.com>
2026-02-08 23:11:11 -0600

    Update 1100

--- main/build/urls.py
+++ main/build/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('app.urls')),
+    # path('', include('app.urls')),
+    path("", views.hello_world, name="hello_world"),
+    path("healthz/", views.healthz_view, name="healthz"),
 ]
 
